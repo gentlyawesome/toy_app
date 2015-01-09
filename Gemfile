375f39ad59b9db08aa5bae77f1efdc517f879e94
Arc Angelo Ibalio <gentlyawesome@gmail.com>
2015-01-09 12:21:05 +0800

    Initialize repository

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'turbolinks'
+  gem 'jbuilder'
+  gem 'sdoc', group: :doc
+end
+
+group :development, :test do 
+ gem 'sqlite3'
+ gem 'spring'
+end
+
+group :production do 
+ gem 'pg'
+ gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = '0007f5bb233175c18376f4739c8ffd9e0ce6c892e7ff80ce0b386c1cc0b217ce2c6e8a14fc5955e6a48507291d015fa9b555c97de0881d3904167b06b83a3dfe'
